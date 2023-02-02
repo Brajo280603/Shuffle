daf1d8093d81f081da0f627710d2201c8e8481e2
Brajananda Mahata <Brajanandamahato280603@gmail.com>
2023-02-03 00:16:17 +0530

    refresh control

--- webpage/script.js
+++ webpage/script.js
@@ -2,5 +2,24 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
 let ele = document.getElementById("link")
-  let songnum = getRandomInt(songs.length-1);
-  ele.href = songs[songnum];
+let refresh = document.getElementById("refresh");
+let songnum = getRandomInt(songs.length-1);
+
+ele.addEventListener('click',()=>{
+    if(refresh.checked == true){
+        // console.log("yes");
+        ele.href = songs[songnum];
+    }else{
+        // console.log("no")
+        songnum = getRandomInt(songs.length-1);
+        ele.href = songs[songnum];
+    }
+})
+
+
+
+ 
+  
+  //add a option for saving to localstorage favorite songs
+
+  
